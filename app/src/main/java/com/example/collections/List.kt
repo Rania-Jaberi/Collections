75a91fd47845519ceac3b96cc603dc4f9e5276d6
Rania-Jaberi <raniajaberi31@gmail.com>
2024-02-14 20:02:57 +0100

    Accéder aux éléments d'une liste

---
+++ app/src/main/java/com/example/collections/List.kt
@@ -0,0 +1,13 @@
+package com.example.collections
+
+class List {
+}
+
+fun main() {
+    val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println(solarSystem.size)
+    println(solarSystem[2])
+    println(solarSystem.get(3))
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto"))
+}
